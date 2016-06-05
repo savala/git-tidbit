8b96f0ac0d1135fef9880d56ba9945892bc7f827
theleastinterestingcoder <quanzhou64@gmail.com>
2016-06-05 14:30:53 -0400

    Revert "integrating email feature"
    This reverts commit 229d3635a5c23e886e260c38f500127bafd9de24.

--- git_fact.py
+++ git_fact.py
@@ -1,19 +1,11 @@
 import sys
 sys.path.append('Snapple')
-sys.path.append('Email')
 
 from termcolor import colored
 from snapple_service import *
-from fetchEmail import *
-from setup import *
-from datetime import *
-
 
 service = SnappleFactService()
 tidbit = service.get_tidbit()
-mailService = setup.get_credentials()
-email = fetchEmail.getEmails(mailService, datetime.now())
 
 print ""
 print colored(tidbit, 'green')
-print colored(email, 'red')
